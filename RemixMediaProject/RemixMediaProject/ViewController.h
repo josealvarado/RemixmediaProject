8681903a5b0a90a55bf8e85db90a14791cd92f58
josealvarado <josealvarado@Joses-MacBook-Pro-2.local>
2015-05-26 19:58:22 -0700

    Initial Commit

---
+++ RemixMediaProject/RemixMediaProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemixMediaProject
+//
+//  Created by Jose Alvarado on 5/26/15.
+//  Copyright (c) 2015 Jose Alvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
